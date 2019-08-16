0bb6ef7acb5ce4d0b62c1fa88830d84438eaf47c
Irene Mateos <irene.mateos@outlook.es>
2019-08-16 21:32:15 +0200

    Añadida la función atacar (work in progress)

--- Prueba1.py
+++ Prueba1.py
@@ -2,20 +2,39 @@
 """
 Created on Fri Aug 16 18:46:55 2019
 
-@author: Raul Milla
+@author: Raul Milla, Irene Mateos
 """
 import random
 import pandas as pd
-personajes=pd.read_excel("Personajes.xlsx")
-personajes
-t=personajes.at[0,'Usuario']
 
 
+estados = [luchando, andando, durmiendo, hablando]
+
+#Importación de los stats de los personajes y los stats generales de los monstruos
+personajes = pd.read_excel("Personajes.xlsx")
+monstruos = pd.read_excel("Monstruos.xlsx")
+
+
+t=personajes.at[0,'Usuario'] #Esto se usa?
+
+#Declaración de las funciones
 def competir(personaje1,personaje2,cualidad,cualidad2):
-    Tirada=random.randit(0,20)+personajes.at[personaje1,cualidad]
-    Tirada2=random.randit(0,20)+personajes.at[personaje2,cualidad]
+    Tirada = random.randit(0,20)+personajes.at[personaje1,cualidad]
+    Tirada2 = random.randit(0,20)+personajes.at[personaje2,cualidad]
     if Tirada<Tirada2 :
         resultado=0
         print("Has fracasado")
     if Tirada>Tirada2 :
         resultado=1("Has triunfado")
+
+
+def atacar(atacante, defensor, arma)
+    contendientes = pd.read_excel("Batalla.xlsx")
+    tirada_ataque = random.randit(0,20) + contendientes.at[atacante,bhabilidad] + contendientes.at[atacante,bcompetencia]
+    tirada_defensa = random.randit(0,20) + contendientes.at[defensor,cualidad] + contendientes.at[atacante,bcompetencia]
+    if tirada_ataque < tirada_defensa:
+        resultado = 0
+        print("El ataque no ha tenido éxito")
+    else:
+        print("El ataque es exitoso!")
+        #consultando el arma hay que saber qué dados se tiran
