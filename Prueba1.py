bfccfee91d62f7307ba496fea68ed8d9c992ee64
Raúl Milla <raulmillagarcia42@gmail.com>
2019-08-17 02:53:11 +0200

    Añadido el menu,ahora toca ver si se puede añadir filas

--- Prueba1.py
+++ Prueba1.py
@@ -6,16 +6,19 @@ Created on Fri Aug 16 18:46:55 2019
 """
 import random
 import pandas as pd
-
-
-estados = [luchando, andando, durmiendo, hablando]
-
-#Importación de los stats de los personajes y los stats generales de los monstruos
+import os
+#Insertamos dataframe
 personajes = pd.read_excel("Personajes.xlsx")
 monstruos = pd.read_excel("Monstruos.xlsx")
+campodebatalla=pd.read_excel("Campo de batalla.xlsx")
+campodebatalla.at[3,'Mounstruo']=3
+
+#estados = [luchando, andando, durmiendo, hablando]
+menu()
+#Creacion de menu
 
 
-t=personajes.at[0,'Usuario'] #Esto se usa?
+#•t=personajes.at[0,'Usuario'] #Esto se usa?
 
 #Declaración de las funciones
 def competir(personaje1,personaje2,cualidad,cualidad2):
@@ -28,7 +31,7 @@ def competir(personaje1,personaje2,cualidad,cualidad2):
         resultado=1("Has triunfado")
 
 
-def atacar(atacante, defensor, arma)
+def atacar(atacante, defensor, arma):
     contendientes = pd.read_excel("Batalla.xlsx")
     tirada_ataque = random.randit(0,20) + contendientes.at[atacante,bhabilidad] + contendientes.at[atacante,bcompetencia]
     tirada_defensa = random.randit(0,20) + contendientes.at[defensor,cualidad] + contendientes.at[atacante,bcompetencia]
@@ -38,3 +41,30 @@ def atacar(atacante, defensor, arma)
     else:
         print("El ataque es exitoso!")
         #consultando el arma hay que saber qué dados se tiran
+def menu():
+	os.system('cls')
+	print ("Selecciona una opción")
+	print ("\t1 - Actualizar ")
+	print ("\t2 - Empezar partida")
+	print ("\t3 - salir")
+while True:
+    opcion=input("Que deseas hacer?")
+    if opcion=="1":
+        print("")
+        input("Has seleccionado actualizar")
+        actualizar()    
+    elif opcion=="2":
+        print("")
+        input("Empezando partida")
+        
+        
+    elif opcion=="3":
+        break
+    else: 
+        print("")
+        input("DING DONG YOU ARE MR WRONG...")
+def actualizar():
+       personajes = pd.read_excel("Personajes.xlsx")
+       monstruos = pd.read_excel("Monstruos.xlsx")
+       print("La lista de jugadores y monstruos ha sido actualizada")
+       print("\n Volviendo al menu principal")
