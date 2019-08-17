d17bd9340776f52e9b321afc730fd39358f7889b
Irene Mateos <irene.mateos@outlook.es>
2019-08-17 12:49:21 +0200

    Mejorada función menú

--- Prueba1.py
+++ Prueba1.py
@@ -14,8 +14,8 @@ campodebatalla=pd.read_excel("Campo de batalla.xlsx")
 campodebatalla.at[3,'Mounstruo']=3
 
 #estados = [luchando, andando, durmiendo, hablando]
-menu()
-#Creacion de menu
+
+
 
 
 #•t=personajes.at[0,'Usuario'] #Esto se usa?
@@ -42,29 +42,42 @@ def atacar(atacante, defensor, arma):
         print("El ataque es exitoso!")
         #consultando el arma hay que saber qué dados se tiran
 def menu():
-	os.system('cls')
-	print ("Selecciona una opción")
-	print ("\t1 - Actualizar ")
-	print ("\t2 - Empezar partida")
-	print ("\t3 - salir")
-while True:
-    opcion=input("Que deseas hacer?")
-    if opcion=="1":
-        print("")
-        input("Has seleccionado actualizar")
-        actualizar()    
-    elif opcion=="2":
-        print("")
-        input("Empezando partida")
-        
-        
-    elif opcion=="3":
-        break
-    else: 
-        print("")
-        input("DING DONG YOU ARE MR WRONG...")
+    #os.system('cls')
+    partida_pausa = 1
+    while partida_pausa == 1:
+            print ("Selecciona una opción")
+            print ("\t1 - Actualizar ")
+            print ("\t2 - Empezar partida")
+            print ("\t3 - salir")
+            opcion_elegida = 0
+            while opcion_elegida == 0:
+                opcion = input("Qué deseas hacer? \nElige un número: ")
+                if opcion=="1":
+                        print("")
+                        input("Has seleccionado actualizar")
+                        actualizar()
+                        opcion_elegida = 1
+                elif opcion=="2":
+                        print("")
+                        input("Empezando partida")
+                        opcion_elegida = 1
+                        partida_pausa = 0
+                elif opcion=="3":
+                        print("Saliendo de la partida")
+                        opcion_elegida = 1
+                        partida_pausa = 0
+                else: 
+                        print("")
+                        input("DING DONG YOU ARE MR WRONG...")
+
+            
 def actualizar():
        personajes = pd.read_excel("Personajes.xlsx")
        monstruos = pd.read_excel("Monstruos.xlsx")
        print("La lista de jugadores y monstruos ha sido actualizada")
        print("\n Volviendo al menu principal")
+
+
+#######FUNCIÓN PRINCIPAL##############
+#Creacion de menu
+menu()
--- PruebaMenu.py
+++ PruebaMenu.py
@@ -5,7 +5,6 @@ Created on Fri Aug 16 22:53:02 2019
 @author: Raul Milla
 """
 import os
-menu()
 def menu():
 	"""
 	Función que limpia la pantalla y muestra nuevamente el menu
@@ -16,3 +15,6 @@ def menu():
 	print ("\t2 - segunda opción")
 	print ("\t3 - tercera opción")
 	print ("\t9 - salir")
+
+	
+menu()
